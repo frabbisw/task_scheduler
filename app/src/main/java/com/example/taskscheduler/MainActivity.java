7fe52530e7b49c3d47380cc95156d63905c4aa07
Fazle Rabbi <frduiit@gmail.com>
2020-01-01 00:31:19 +0600

    init

--- app/src/main/java/com/example/taskscheduler/MainActivity.java
+++ app/src/main/java/com/example/taskscheduler/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.taskscheduler;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    protected Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
